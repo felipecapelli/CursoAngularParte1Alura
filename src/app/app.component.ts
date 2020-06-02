6b849a1680f55cdd86c64a4a9bede95f21c25568
felipecapelli <felipe.ccapelli@gmail.com>
2020-06-02 09:05:00 -0300

    Aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+  title = 'lala';
+  description = 'Leao';
+  url = 'https://d168rbuicf8uyi.cloudfront.net/wp-content/uploads/2019/06/13145802/sonhar-com-leao-1024x649.jpg';
 }
